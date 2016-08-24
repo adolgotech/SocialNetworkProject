bda673dd1f4087e856c9c7fdd8a3e9052bae89fe
dtaylor <adolgo.technologies@gmail.com>
2016-08-24 17:09:16 -0600

    removed body-parser to server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.use(bodyParser.json());
-
 app.use("/api/posts", require("./controllers/api/posts"));
 app.use(require("./controllers/static"));
 
