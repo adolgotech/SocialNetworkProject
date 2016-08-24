30f330d23270f21e5e2c6353a4610a42824dc096
dtaylor <adolgo.technologies@gmail.com>
2016-08-24 13:10:50 -0600

    fixing post.html issue

--- controllers/static.js
+++ controllers/static.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-  res.sendfile("../layouts/post.html", {"root" : __dirname});
+  res.sendfile("layouts/post.html");
 });
 
 router.use(express.static(__dirname + "/../assets"));
