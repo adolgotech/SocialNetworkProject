d26294e6043753065b1104a4cd6d201793e0a772
dtaylor <adolgo.technologies@gmail.com>
2016-08-24 13:00:44 -0600

    finding layouts/posts.html issue

--- controllers/static.js
+++ controllers/static.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-  res.sendfile(__dirname + "/../layouts/post.html");
+  res.sendfile("../layouts/post.html", {"root" : __dirname});
 });
 
 router.use(express.static(__dirname + "/../assets"));
