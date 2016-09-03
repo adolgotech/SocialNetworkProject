fe3ff8fc82736576af172e45166984d63639ba4a
adolgotech <adolgo.technologies@gmail.com>
2016-09-03 17:20:12 -0600

    changed websocket implementation into a service

---
+++ ng/websockets.svc.js
@@ -0,0 +1,36 @@
+angular.module('app')
+.service('WebSocketSvc', function ($rootScope, $timeout) {
+  var connection;
+  function websocketHost() {
+    if($window.location.protocol === "https:") {
+      return "wss://" + $window.location.host;
+    } else {
+      return "ws://" + $window.location.host;
+    }
+  }
+  
+  this.connect = function () {
+    connection = new WebSocket(websocketHost());
+    connection.onclose = function (e) {
+      console.log('WebSocket closed. Reconnecting...');
+      $timeout(connect, 10000);
+    }
+
+    connection.onopen = function (){
+      console.log('WebSocket connected');
+    }
+
+    connection.onmessage = function (e) {
+      console.log(e);
+      var payload = JSON.parse(e.data);
+      $rootScope.$broadcast('ws:' + payload.topic, payload.data);
+    }
+  }
+
+  this.send = function (topic, data) {
+    var json = JSON.stringify({topic: topic, data: data});
+    connection.send(json);
+  }
+}).run(function (WebSocketSvc) {
+    WebSocketSvc.connect();
+})
